faa8bf7e233c9c3bba950867ea529837e51ff074
FelipeBurlim <feeleburlim31@gmail.com>
2025-10-07 16:33:07 -0300

    Escolhas do jogador A

--- src/assets/content/arena.ts
+++ src/assets/content/arena.ts
@@ -3,10 +3,10 @@ import type { ArenaState } from "_types/arena";
 
 export const areaContent: ArenaState[] = [
   {
-    attribute: "aura",
-    deckA: "a-carlos",
-    deckB: "b-jorge",
-    playerAName: "Player A",
+    attribute: "estilo",
+    deckA: "b-jorge",
+    deckB: "a-carlos",
+    playerAName: "Burlim",
     playerBName: "Player B",
   },
   // apenas modifique a estrutura atual
